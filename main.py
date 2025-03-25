d855763a555e33ae76c10405bce1cba233524a00
Nandan <193489667+nandano@users.noreply.github.com>
2025-03-25 20:20:38 +0530

    Add few extra questions
    Also added a variable that finds the total number of questions instead of specifying a number as a string

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ questions = [
     {"question": "What is the largest ocean on Earth?", "options": ["A. Atlantic Ocean", "B. Indian Ocean", "C. Arctic Ocean", "D. Pacific Ocean"], "answer": 'D'},
     {"question": "Which animal is known as the \"King of the Jungle\"?", "options": ["A. Tiger", "B. Elephant", "C. Lion", "D. Gorilla"], "answer": 'C'},
     {"question": "What is the Chemical symbol for Gold?", "options": ["A. Au", "B. Ag", "C. Pb", "D. Go"], "answer": 'A'},
+    {"question": "What year did the Titanic sink?", "options": ["A. 1912", "B. 1921", "C. 1905", "D. 1898"], "answer": 'A'},
+    {"question": "What is the only metal that is liquid at room temperature?", "options": ["A. Gallium", "B. Mercury", "C. Cesium", "D. Lead"], "answer": 'B'},
+    {"question": "What is the smallest country in the world by land area?", "options": ["A. Monaco", "B. Nauru", "C. Vatican City", "D. San Marino"], "answer": 'C'},
+    {"question": "What was the first-ever video game?", "options": ["A. Pong", "B. Space Invaders", "C. Tennis for Two", "D. Pac-Man"], "answer": 'C'},
+    {"question": "Which element is named after the greek for \"green\"?", "options": ["A. Greenium", "B. Beryllium", "C. Oxygen", "D. Chlorine"], "answer": 'D'},
+    {"question": "What was the first country to grant women the right to vote?", "options": ["A. USA", "B. New Zealand", "C. France", "D. Sweden"], "answer": 'B'},
 ]
 
 random.shuffle(questions)
@@ -26,9 +32,10 @@ for question in questions:
     else:
         print("Oh no you got it wrong.")
 
-if score <= 2:
-    print(f"\n{score} / 6\nKeep trying!")
-elif score <= 4:
-    print(f"\n{score} / 6.\nGood job!")
+questions_no = len(questions)
+if score <= 4:
+    print(f"\n{score} / {questions_no}.\nKeep trying!")
+elif score <= 8:
+    print(f"\n{score} / {questions_no}.\nGood job!")
 else:
-    print(f"\n{score} / 6\nYou're a quiz master!")
+    print(f"\n{score} / {questions_no}.\nYou're a quiz master!")
